08df25bc7680156601699afee52bcabfb0d2e80e
TIBOR BIRO <biro.tibor99@gmail.com>
2023-11-23 14:04:39 +0100

    second commit

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,15 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Popup",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/Dialog",
+	"sap/m/Button",
+	"sap/m/library",
+	"sap/m/List",
+	"sap/m/StandardListItem",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Popup, JSONModel, Dialog, Button, mobileLibrary, List, StandardListItem, Fragment) {
         "use strict";
 
         return Controller.extend("Thesis.thesis.controller.Login", {
             onInit: function () {
 
+            },
+
+            onDialogPress: function () {
+                debugger;
+                if (!this._uDialog) {
+                    this._uDialog = this.loadFragment({
+                        name: "Thesis.thesis.view.Registration"
+                    });
+                }
+                this._uDialog.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
+
+            onCloseDialong:function(){
+                this.byId("Registration").close();
             }
+
         });
     });
